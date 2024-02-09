8380a01d46d2ad9123a3f5df7d13aec9f29cc65c
Paco Mañó Frasquet <110739314+framafra@users.noreply.github.com>
2024-02-09 17:22:51 +0100

    Update config.php

--- app/config.php
+++ app/config.php
@@ -5,6 +5,6 @@ define('DB_NAME', 'db');
 define('DB_USER', 'user');
 define('DB_PASSWORD', 'password');
 
-$mysqli = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+$mysqli = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 ?>
